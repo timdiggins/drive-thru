d6e89a2510017ed69c755cd7c18b9d5c1fafbba5
Tim Diggins <tim@red56.co.uk>
2010-09-02 14:31:38 +0100

    removing site-specific stuff

--- config/dna.rb
+++
@@ -1,6 +0,0 @@
-require 'rubygems'
-require 'json'
-
-dna = {}
-
-open(File.dirname(__FILE__) + "/dna.json", "w").write(dna.to_json)
--- config/solo.rb
+++
@@ -1,2 +0,0 @@
-file_cache_path    "/var/chef"
-cookbook_path      ["/var/chef/site-cookbooks", "/var/chef/cookbooks"]
